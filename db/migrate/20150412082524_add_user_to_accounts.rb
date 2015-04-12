0566282293a7ae07512419c9f2486c61c7ada6cf
Mike Soikkeli <msoikkel@cisco.com>
2015-04-12 02:29:48 -0600

    assoc users w accounts

---
+++ db/migrate/20150412082524_add_user_to_accounts.rb
@@ -0,0 +1,5 @@
+class AddUserToAccounts < ActiveRecord::Migration
+  def change
+    add_column :users, :account_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150412082033) do
+ActiveRecord::Schema.define(version: 20150412082524) do
 
   create_table "accounts", force: true do |t|
     t.datetime "created_at"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20150412082033) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.integer  "account_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
